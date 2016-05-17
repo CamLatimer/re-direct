c8ea6be1ba6562b6391202ee23128c2ae5d4b29f
Cam <camlatimer@gmail.com>
2016-05-16 21:13:13 -0400

    rearranged naming convention for urls and added a script file to do client side AJAX

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is intended to be a url-shortener.  Gonna try to give it my own twist though.
 I know there's a ton out there already.
 
-If you want, fork and/or download, check out the dependencies in package.json to see what you
-need, then run npm --install.
+Feel free to work on it. Fork and/or download, check out the dependencies in package.json to see what you
+need, then run npm --install.  Make it work good, shoot me some feedback, etc.
+
+
+--Cam
--- index.js
+++ index.js
@@ -6,39 +6,45 @@ var parser = require('body-parser');
 app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(parser.urlencoded({extended: true}));
-app.use(parser.json({extended: true}));
+// app.use(parser.json({extended: true}));
 app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), function(){
   console.log("server runnin'");
 });
 
+// model
 var Link = Schema.Link;
 
-app.get('/linke_me_dude', function(req, res){
-  res.send('./public/index.html');
-})
-app.get('/link_me_dude/yo_link/:customizr', function(req, res){
-  res.send('gotcha, here is the link: ' + '<a href="/link_me_dude/'+req.params.customizr+'">link_me_dude/'+req.params.customizr+'</a>');
-});
-app.get('/link_me_dude/:customizr', function(req, res){
-  Link.findOne({customizr: req.params.customizr}, function(err, link){
-    res.redirect(link.input_url);
-  });
-});
+// routing
 
+// home page. express looks for index.html in the root dir when pointed to '/'
+app.get('/link_me_dude', function(req, res){
+  res.redirect('/');
+});
+// check out all the links in db
 app.route('/links')
 .get(function(req, res){
   Link.find({}).then(function(links){
     res.json(links);
   });
 })
+// add to link to the db and redirect to
+// a page showing the user's new url
 .post(function(req, res){
   Link.create(req.body, function(err, link){
     if(err){
       console.log(err);
     }else{
       console.log('whoa, you just created: ' + link);
-      res.redirect('/link_me_dude/yo_link/' + link.customizr);
+      res.redirect('/your_link_is/' + link.customizr);
     }
+  });
+});
+app.get('/your_link_is/:customizr', function(req, res){
+  res.send('gotcha, here is the link: ' + '<a href="/'+req.params.customizr+'">' +'/'+req.params.customizr+'</a>');
+});
+app.get('/:customizr', function(req, res){
+  Link.findOne({customizr: req.params.customizr}, function(err, link){
+    res.redirect(link.input_url);
   });
 });
